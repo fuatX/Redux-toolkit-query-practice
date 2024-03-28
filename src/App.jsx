133240c50637f5871a2841423febdf6929d4a4c9
fuatX <fuatyagizaydin@hotmail.com>
2024-03-29 00:05:00 +0300

    Personal-todo-and-navbar-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Navbar from "./compenents/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbar />
       <h1>App</h1>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+/*
+TODO: Bootstrap Nav Yapisi Olusturulacak
+TODO: React Router DOM kur
+TODO: Ana Sayfa, Her Api icin Kategori Olustur.
+TODO: .env yapisi olustur ve Icıne Bilgi Ekle
+TODO: Container ve Row için Component Olustur.
+TODO: Cat APIdan Key Al
+TODO: Cat API Yapisini Fetch ile Kullan
+TODO: ilsLoading, isError, error ve data yapisini kurgula
+TODO: Cat API Custom Hook yap
+TODO: Cat API Kategori Detay Sayfasi Yap
+TODO: UI Düzenle
+TODO: Axious ile Ayni Calismalari Tekrarla
+TODO: RTK Query ile Ayni Yapiyi Kurgula
+*/
